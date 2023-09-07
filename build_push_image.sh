d52ae61a1455e8644ccf01bb81c9fc464ed90006
BoyNovr <133074816+BoyNovr@users.noreply.github.com>
2023-09-07 15:19:22 +0700

    Add files via upload

---
+++ build_push_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Membuat docker image dengan nama item-App:v1
+docker build -t item-app:v1 .
+
+#melihat daftar image 
+docker images
+
+#Mengubah nama image agar sesuai dengan format Docker Hub
+docker tag item-app:v1 boynovriyal/item-app:v2
+
+#jalankan perintah docker login
+echo $PASSWORD_DOCKER_HUB | sudo docker login -u boynovriyal --password-stdin
+
+#mengunggah image ke Docker Hub
+sudo docker push boynovriyal/item-app:v2
+
