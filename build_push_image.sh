fb3922b0096de30dd3ab116be05d01bee8862c87
BoyNovr <133074816+BoyNovr@users.noreply.github.com>
2023-09-07 17:47:54 +0700

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,11 +7,10 @@ docker build -t item-app:v1 .
 docker images
 
 #Mengubah nama image agar sesuai dengan format Docker Hub
-docker tag item-app:v1 boynovriyal/item-app:v2
+docker tag item-app:v1 boynovriyal/item-app:v1
 
 #jalankan perintah docker login
 echo $PASSWORD_DOCKER_HUB | sudo docker login -u boynovriyal --password-stdin
 
 #mengunggah image ke Docker Hub
-sudo docker push boynovriyal/item-app:v2
-
+sudo docker push boynovriyal/item-app:v1
